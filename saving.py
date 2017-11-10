b9ca7f8904b08b1a976fe76fb13721185da93c1a
koiso <heppu.seuranen@gmail.com>
2017-11-10 14:11:29 +0200

    Add files via upload

---
+++ saving.py
@@ -0,0 +1,15 @@
+﻿"""Tämä luokka vastaa tiedostonkäsittelystä. 
+sisältää metodit kks-parametrien tallennukseen, kks-tallennussanomien tallennukseen, aloituskenttien sisällön tallentamiseen,
+kalibrointitiedot, kielivalinnat , tallennuspolku, kommunikaation hallinta(portti, baudi) ..."""
+
+
+import sys, time
+
+class Saving (Object):
+
+	def __init__ (self):
+
+		
+	def tallennaParametrit (self, params):
+
+
