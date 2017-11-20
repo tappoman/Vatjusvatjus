dd11303ed2b66d69410ea543903dc3ed30b85b26
Jussi Sepponen <jussi.sepponen@student.oulu.fi>
2017-11-20 14:28:17 +0200

    barit toimii, testidatan kirjotus

--- bar_animation.py
+++ bar_animation.py
@@ -1,31 +1,128 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 
-x = []
-y = []
+style.use('fivethirtyeight')
+
+puolikierrokset =  []
+voima = []
+syvyys =  []
+fig, ax = plt.subplots()
 
 def linesplitter(line):
 
-    line_x = line[0]
-    line_y = line[1]
-    return line_x, line_y
+        linex = int(line[0])
+        liney = int(line[1])
+        liney2 = int(line[2])
+        if liney2 is None:
+            liney2 = 0
+            return linex, liney, liney2
+        else:
+            return linex, liney, liney2
 
-with open("testfile.txt", "r") as testfile:
+def get_values(i):
+    if voima[i] != 0:
+        return syvyys[i], voima[i]
+    else:
+        return syvyys[i], puolikierrokset[i]
 
-    for line in testfile:
-        lineparts = line.replace('\n', '').split(',')
-        line_x, line_y = linesplitter(lineparts)
-        x.append(line_x)
-        y.append(line_y)
-    print("x-akselille: "+str(x))
-    print("y-akselille: "+str(y))
+def read_values():
+    with open("testfile.txt", "r") as testfile:
+        for line in testfile:
+            if len(line) > 1:
+                lineparts = line.replace('\n', '').split(',')
+                if len(lineparts) == 2:
+                    lineparts.append(0)
+                line_s, line_pk, line_v = linesplitter(lineparts)
+                puolikierrokset.append(line_pk)
+                voima.append(-line_v)
+                syvyys.append(line_s)
 
-fix, ax = plt.subplots()
+        #print("syvyys: " + str(syvyys))
+        #print("pkierr: " + str(puolikierrokset))
+        #print("voima: " + str(voima))
 
-fig = plt.barh(x, y)
+def chart_setup_vertical():
+    #fig, ax = plt.subplots()
 
-plt.gca().invert_yaxis()
-ax.set_xticks(range(0,100,20))
+    # x_labels = range(0, 500, 20)
+    ax.axhline(syvyys == 0, color='k')
+
+    plt.xlabel('syvyys')
+    plt.ylabel('voima           puolikierrokset')
+
+    # ax.set_xticklabels(syvyys, rotation = 90)
+    ax.xaxis.label.set_rotation(90)
+    for label in ax.xaxis.get_ticklabels():
+        label.set_rotation(90)
+    for label in ax.yaxis.get_ticklabels():
+        label.set_rotation(90)
+    plt.subplots_adjust(left = 0.12, bottom = 0.3, right = 0.94, top = 0.90, wspace = 0.2, hspace = 0)
+
+def chart_setup_horizontal():
+
+    plt.gca().invert_yaxis()
+    ax.axvline(syvyys == 0, color='k')
+    plt.ylabel('syvyys           ')
+    ax.annotate('voima / puolikierrokset',
+                xy=(-30, -2))
+    ax.yaxis.label.set_rotation(360)
+    plt.subplots_adjust(left = 0.25, bottom = 0.15, right = 0.94, top = 0.90, wspace = 0.2, hspace = 0.2)
+
+'''
+def draw_graph():
+
+    p = 0
+    for s in syvyys:
+        if voima[p] != 0:
+            ax2.bar(syvyys[p], voima[p], width=5, align='center', color='g', label='voima')
+            p = p + 1
+        else:
+            ax.bar(syvyys[p], puolikierrokset[p], width=5, align='center', color='b', label='puolikierrokset')
+            p = p + 1
+'''
+
+def animate_horizontal(i):
+
+    read_values()
+    p = 0
+    for s in syvyys:
+        if voima[p] != 0:
+            ax.barh(syvyys[p], width=voima[p], height=10, color='g')
+            p = p + 1
+        else:
+            ax.barh(syvyys[p], width=puolikierrokset[p], height=10, color='b')
+            p = p + 1
 
 
-plt.show()
+def animate_vertical(i):
 
+    read_values()
+    p = 0
+    for s in syvyys:
+        if voima[p] != 0:
+
+            ax.bar(syvyys[p], voima[p], width=5, align='center', color='g')
+            p = p + 1
+        else:
+            ax.bar(syvyys[p], puolikierrokset[p], width=5, align='center', color='b')
+            p = p + 1
+
+
+
+def main():
+
+    #vertical
+    #chart_setup_vertical()
+    #anim = animation.FuncAnimation(fig, animate_vertical, interval=2000)
+
+
+    #horizontal
+    chart_setup_horizontal()
+    anim = animation.FuncAnimation(fig, animate_horizontal, interval=2000)
+    plt.show()
+
+
+# muutit syvyyden piirron ax2
+
+main()
--- writing_test.py
+++ writing_test.py
@@ -3,10 +3,10 @@ import random
 testitiedosto = open("testfile.txt", "w")
 
 def luo_satunnaiset_puolikierrokset():
-    satunnaispuolikierrokset =  random.randint(-20,150)
+    satunnaispuolikierrokset =  random.randint(0, 100)
     return satunnaispuolikierrokset
 
-'''
+
 def luo_satunnaisvoima():
     satunnaisarvo = random.randint(0,5)
     if satunnaisarvo >= 4:
@@ -14,26 +14,26 @@ def luo_satunnaisvoima():
         return satunnaisvoima
     else:
         return False
-'''
 
-arvojen_maara = range(5)
-syvyys_alku = 0
+
+arvojen_maara = range(15)
+syvyys_alku = 20
 
 for i in arvojen_maara:
     puolikierrokset_kirjoita = luo_satunnaiset_puolikierrokset()
-    #voima_kirjoita = luo_satunnaisvoima()
-    #if voima_kirjoita == False:
-    print("syvyys:"+str(syvyys_alku)+",","toinen_arvo:"+str(puolikierrokset_kirjoita)+'\n')
-    testitiedosto.write(str(syvyys_alku))
-    testitiedosto.write("," + str(puolikierrokset_kirjoita) + '\n')
-    syvyys_alku = syvyys_alku + 20
-    '''
+    voima_kirjoita = luo_satunnaisvoima()
+    if voima_kirjoita == False:
+        print("syvyys:"+str(syvyys_alku)+",","toinen_arvo:"+str(puolikierrokset_kirjoita)+'\n')
+        testitiedosto.write(str(syvyys_alku))
+        testitiedosto.write("," + str(puolikierrokset_kirjoita) + '\n')
+        syvyys_alku = syvyys_alku + 20
+
     else:
         print("syvyys:" + str(syvyys_alku) + ",", "puolikierrokset:" + str(puolikierrokset_kirjoita)+ ",", "voima:" + str(voima_kirjoita) + '\n')
         testitiedosto.write(str(syvyys_alku))
         testitiedosto.write(","+str(puolikierrokset_kirjoita))
         testitiedosto.write(","+str(voima_kirjoita)+'\n')
         syvyys_alku = syvyys_alku+20
-    '''
+
 
 testitiedosto.close()
